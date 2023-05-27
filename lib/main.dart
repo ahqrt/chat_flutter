a1f3c644ac3518cc7fc453a2419e093f3d41af4f
yangliye <1217223113@qq.com>
2023-05-27 10:06:59 +0800

    chore: init

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:chat_flutter/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
